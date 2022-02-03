093a4a3bc1f10e2fc8782e296a41ed27b7d114fd
Tamzid Ahmad <tahmadschool@gmail.com>
2022-02-03 20:13:59 +0000

    fifth

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
-    var years = new Date().getFullYear()+parseInt(years);
+    var year = new Date().getFullYear()+parseInt(years);
     var amount = interest+principal;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+(years+2020)+"\<br\>";
+    document.getElementById("principal").focus();
+    if(parseInt(principal) <= 0){
+        document.getElementById("result").innerHTML="Enter a positive Number";
+    }
+    else{
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+(year)+"\<br\>";
+}
 }
 
 function updateRate()
