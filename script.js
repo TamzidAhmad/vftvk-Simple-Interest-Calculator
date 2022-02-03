3ba960a24c00b6c8afb1c21cad9b6b686e772940
Tamzid Ahmad <tahmadschool@gmail.com>
2022-02-03 21:21:34 +0000

    revert

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
-    var amount = parseInt(interest).toFixed(2);
+    var amount = (interest+principal).toFixed(2);
     document.getElementById("principal").focus();
     if(parseInt(amount) <= 0){
         document.getElementById("result").innerHTML="Enter a positive Number";
     }
     else{
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+<mark>rate</mark>+"%\<br\>You will receive an amount of "+<mark>amount</mark>+" from interest,\<br\>in the year "+<mark>year</mark>+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
 }
 }
 
@@ -20,4 +20,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-        
